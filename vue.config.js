dc60b3a7a29c1c5d7cb0b680898adf2e8ec15a73
zhenzipu <zhenzipu@jd.com>
2019-04-03 18:01:35 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const webpack = require("webpack");
 module.exports = {
   // 基本路径
-  baseUrl: "/tree-edit-drag/docs/",
+  publicPath: "/tree-edit-drag/",
   // 输出文件目录
   outputDir: "docs",
   configureWebpack: {
